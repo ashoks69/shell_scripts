31542c7922cd87f9deffe31072194f88f2c0fdc7
ashok <ashok93.ece@gmail.com>
2025-01-02 23:16:39 +0530

    commiting

---
+++ 07.special_vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "How many vars are passed: $#"
+echo "all vars passed $@"
+echo "Script name is $0"
+echo "Preset working Dir $PWD"
+echo "Home dir of current user : $HOME"
+echo "which user is runninght scrtpt $USER"
+echo "process id of current script $$"
+echo "process id of last command in the background $!"  
