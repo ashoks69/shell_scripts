860f047db1e26176ea5febd75b47319c2fa3cea7
ashok <ashok93.ece@gmail.com>
2025-01-02 21:49:48 +0530

    commiting

--- 04.vars.sh
+++ 04.vars.sh
@@ -4,10 +4,10 @@ echo "Please enter your username : " # this will take the data from user
 
 read -s USERNAME # this will wait untill user gives the input
 
-echo "Welcome $USERNAME"
+#echo "Welcome $USERNAME"
 
 echo "Please enter your Password : " # this will take the data from user
 
 read -s PASSWORD # this will wait untill user gives the input
 
-echo "Password is $PASSWORD"
+#echo "Password is $PASSWORD"
