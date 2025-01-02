936d17f92cfe4b35c4bc9b02f1f188c6d6642a52
ashok <ashok93.ece@gmail.com>
2025-01-02 20:40:39 +0530

    adding Vars

---
+++ 02.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ashok
+PERSON2=Vani
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2 : Hello $PERSON1"
+echo "$PERSON1: How are you doing $PERSON2"
+echo "$PERSON2 : I am doing good $PERSON1"
