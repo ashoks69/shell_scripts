23b9cf25175a8a33742878a76ae0595c9c94062e
ashok <ashok93.ece@gmail.com>
2025-01-05 17:11:30 +0530

    installing softwares

--- 10.softwares_install.sh
+++ 10.softwares_install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-USERID=$(id -u)
+USERID=$(id -u) #id -u = user id; USERID = variable
 
 if [ $USERID -ne 0 ]
  then
 	echo "ERROR: You must have Admin access to install"
-	exit 1
+	exit 1 # it should be 0 for success and expect zero for failure
 fi
 
-if [ $? -ne 0 ] # 0 means it not installed
+dnf list installed mysql #list all installed software
+if [ $? -ne 0 ] # 0 means it not installed ; -ne = not equal
 	then
 		dnf install mysql -y
 			if [ $? -ne 0 ]
@@ -21,6 +22,7 @@ if [ $? -ne 0 ] # 0 means it not installed
 		echo "MYSQL is already.....INSTALLED"
 fi
 
+dnf list installed git #list all installed software
 if [ $? -ne 0 ] # 0 means it not installed
 	then
 		dnf install git -y
