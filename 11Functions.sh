2a0062241fb95e1ed40a18557d468b554285b336
ashok <ashok93.ece@gmail.com>
2025-01-05 17:36:30 +0530

    installing softwares

--- 11Functions.sh
+++ 11Functions.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 USERID=$(id -u) #id -u = user id; USERID = variable
-R="\e[31m" #red color
-G="\e[32m" #green color
-Y="\e[33m" #yellow color
-N="\e[0m" #no color
+
+VALIDATE(){
+	if [ $1 -ne 0 ]
+			 then
+				echo  "$2.....FAIL"
+			 else
+				echo "$2.....Success"
+			fi
+			}
+# R="\e[31m" #red color
+# G="\e[32m" #green color
+# Y="\e[33m" #yellow color
+# N="\e[0m" #no color
 
 if [ $USERID -ne 0 ]
  then
@@ -16,26 +25,16 @@ dnf list installed mysql #list all installed software
 if [ $? -ne 0 ] # 0 means it not installed ; -ne = not equal
 	then
 		dnf install mysql -y
-			if [ $? -ne 0 ]
-			 then
-				echo -e "Installing MYSQL.....$R FAIL $N"
-			 else
-				echo "Installing MYSQL.....$G Success $N"
-			fi
+		VALIDATE $? "Installing MYSQL" #function call
 	else
-		echo "MYSQL is already.....$Y INSTALLED $N"
+		echo "MYSQL is already.....INSTALLED"
 fi
 
 dnf list installed git #list all installed software
 if [ $? -ne 0 ] # 0 means it not installed
 	then
 		dnf install git -y
-			if [ $? -ne 0 ]
-			 then
-				echo "Installing GIT.....$R FAIL $N"
-			 else
-				echo "Installing GIT.....$G Success $N"
-			fi
+		VALIDATE $? "Installing Git" #function call
 	else
-		echo "GIT is already.....$Y INSTALLED $N"
+		echo "GIT is already.....INSTALLED"
 fi
